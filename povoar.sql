c5358e8ce695cf7f11a9a46acc6cc279e8e89c86
Xavier-Pisco <x.pisco@gmail.com>
2020-03-31 14:31:12 +0100

    Some more cases to check

--- povoar.sql
+++ povoar.sql
@@ -6,9 +6,11 @@ PRAGMA foreign_keys = ON;
 INSERT INTO Production VALUES(1, "The Lord of The Rings: The Fellowship of The Ring", 2001, "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.", "https://www.youtube.com/watch?v=V75dMMIW2B4");
 INSERT INTO Production VALUES(3, "Joker", 2019, "In Gotham City, mentally troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: the Joker.", "https://www.imdb.com/video/imdb/vi1723318041?playlistId=tt7286456&ref_=tt_ov_vi");
 INSERT INTO Production VALUES(2, "Game of Thrones", 2011, "Nine noble families fight for control over the mythical lands of Westeros, while an ancient enemy returns after being dormant for thousands of years.", "https://www.imdb.com/video/imdb/vi3672620057?playlistId=tt0944947&ref_=tt_ov_vi");
+INSERT INTO Production VALUES(4, "The Dark Knight", 2008, "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", "https://www.imdb.com/video/vi324468761?playlistId=tt0468569&ref_=tt_ov_vi");
 
 INSERT INTO Movie VALUES(1, "2:02:00");
 INSERT INTO Movie VALUES(3, "2:02:00");
+INSERT INTO Movie VALUES(4, "2:32:00");
 INSERT INTO Series Values(2, 2019);
 INSERT INTO Season Values(1, 2, 2011, 2011);
 INSERT INTO Episode Values(1, 1, "Winter Is Coming", "A Night's Watch deserter is tracked down; Lord Eddard 'Ned' Stark learns that his mentor has died; Viserys Targaryen plots to win back the throne; Robert arrives at Winterfell with his family; Ned prepares to leave for King's Landing.", "1:02:00", "2011-10-17");
@@ -19,14 +21,19 @@ INSERT INTO Person VALUES(1, "Rafael Valente Cristino", "2000-08-25");
 INSERT INTO Person VALUES(2, "Joaquin Phoenix", "1974-10-28");
 INSERT INTO Person VALUES(3, "Todd Philips", "1970-12-20");
 INSERT INTO Person VALUES(4, "Bronson Webb", "1983-02-20");
+INSERT INTO Person VALUES(5, "Xavier Pisco", "2000-10-29");
+INSERT INTO Person VALUES(6, "Heath Ledger", "1979-04-04");
 INSERT INTO Celebrity VALUES(2, 1.73, "I like sql");
 INSERT INTO Celebrity VALUES(3, 1.83, "Todd Phillips was born on December 20, 1970 in Brooklyn, New York City, New York, USA as Todd Bunzl. He is a producer and director, known for Joker (2019), A Tempo e Horas (2010) and Dias de Loucura (2003).");
 INSERT INTO Celebrity VALUES(4, NULL, "Bronson Webb was born on February 20, 1983 in London, England as Bronson John Webb. He is an actor, known for RocknRolla: A Quadrilha (2008), Reino dos Céus (2005) and Expiação (2007).");
+INSERT INTO Celebrity VALUES(6, 1.85, "Heath Ledger was born on the fourth of April 1979, in Perth, Western Australia, to Sally and Kim Ledger.");
 INSERT INTO User VALUES(1, "up201806680@fe.up.pt", "verysecret");
+INSERT INTO User VALUES(5, "up201806134@fe.up.pt", "password");
 
 -- ACTOR && ROLE
 
 INSERT INTO Actor VALUES(2, 3, "Joker", "Main Character");
+INSERT INTO Actor VALUES(6, 4, "Joker", "Secondary Character");
 INSERT INTO Role VALUES(3, 3, "Director");
 INSERT INTO SingleEpisodeActor VALUES(4, 1, "Will", "Secondary Character"); -- The dude who broke the Night's Watch oath in the first episode
 
@@ -60,6 +67,8 @@ INSERT INTO BelongsToGenre VALUES(2, 8);
 INSERT INTO BelongsToGenre VALUES(3, 6);
 INSERT INTO BelongsToGenre VALUES(3, 5);
 INSERT INTO BelongsToGenre VALUES(3, 7);
+INSERT INTO BelongsToGenre VALUES(4, 2);
+INSERT INTO BelongsToGenre VALUES(4, 7);
 
 -- AWARD
 
@@ -68,10 +77,16 @@ INSERT INTO AwardType VALUES(2, "Grammy");
 INSERT INTO AwardType VALUES(3, "Golden Globe");
 
 INSERT INTO AwardCategory VALUES(1, "Best Performance by an Actor in a Leading Role");
+INSERT INTO AwardCategory VALUES(3, "Best Performance by an Actor in a Supporting Role");
 INSERT INTO AwardCategory VALUES(2, "Best Performance by an Actor in a Motion Picture");
+INSERT INTO AwardCategory VALUES(4, "Best Motion Picture of the Year");
 
 INSERT INTO CelebrityAward VALUES(2, 1, 1, 2020, 1);
 INSERT INTO ProductionAward VALUES(3, 1, 1, 2020, 1);
+INSERT INTO CelebrityAward VALUES(6, 1, 3, 2009, 1);
+INSERT INTO ProductionAward VALUES(2, 1, 4, 2020, 0);
+INSERT INTO CelebrityAward VALUES(2, 3, 3, 2000, 0);
+
 
 /*select * from Person;
 select * from Celebrity;
@@ -93,7 +108,8 @@ select * from ReviewProduction;
 select * from BelongsToList;
 select * from BelongsToGenre;
 
-select * from CelebrityAward;*/
+select * from CelebrityAward;
+select * from ProductionAward;*/
 
 
 
