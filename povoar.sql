b3fab8ca17c659881b5f27ec19fb70924453e2f8
Jessica <jessymireie@github.com>
2020-04-02 13:31:53 +0100

    More data

--- povoar.sql
+++ povoar.sql
@@ -9,6 +9,7 @@ INSERT INTO Production VALUES(2, "Game of Thrones", 2011, "Nine noble families f
 INSERT INTO Production VALUES(4, "The Dark Knight", 2008, "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", "https://www.imdb.com/video/vi324468761?playlistId=tt0468569&ref_=tt_ov_vi");
 INSERT INTO Production VALUES(5, "Breaking Bad", 2008, "A high school chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine in order to secure his family's future.", NULL);
 INSERT INTO Production VALUES(6, "Harry Potter and the Sorcerer's Stone", 2001, "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.", "https://www.imdb.com/video/imdb/vi3115057433?playlistId=tt0241527&ref_=tt_ov_vi");
+INSERT INTO Production VALUES(7,"Dark",2017,"A family saga with a supernatural twist, set in a German town, where the disappearance of two young children exposes the relationships among four families.","https://www.imdb.com/video/vi496745241?playlistId=tt5753856&ref_=vp_rv_3");
 
 INSERT INTO Movie VALUES(1, TIME("02:02:00"));
 INSERT INTO Movie VALUES(3, TIME("02:02:00"));
@@ -16,11 +17,15 @@ INSERT INTO Movie VALUES(4, TIME("02:32:00"));
 INSERT INTO Movie VALUES(6, TIME("02:32:00"));
 INSERT INTO Series Values(2, 2019);
 INSERT INTO Series Values(5, 2013);
+INSERT INTO Series Values(7,NULL);
 INSERT INTO Season Values(1, 2, 2011, 2011);
 INSERT INTO Season Values(2, 5, 2008, 2008);
+INSERT INTO Season Values(3, 7, 2017, 2017);
 INSERT INTO Episode Values(2, 2, "Pilot", NULL, NULL, NULL);
 INSERT INTO Episode VALUES(3, 2, "Felina", "Walter White makes one last attempt to secure his family's future, while also visiting some old enemies, during his final return to Albuquerque.", NULL, DATE("2013-09-29"));
 INSERT INTO Episode Values(1, 1, "Winter Is Coming", "A Night's Watch deserter is tracked down; Lord Eddard 'Ned' Stark learns that his mentor has died; Viserys Targaryen plots to win back the throne; Robert arrives at Winterfell with his family; Ned prepares to leave for King's Landing.", TIME("01:02:00"), DATE("2011-10-17"));
+
+INSERT INTO Episode VALUES(4,3,"Secrets","The small German town of Winden is shaken by the disappearance of a teenage boy. While the townsfolk are occupied with secrets of their own, at nightfall a group of teenagers attempts to recover something the missing boy may have left behind.",TIME("00:51:00"),DATE("2017-12-01"));
 
 -- PERSON
 
@@ -34,6 +39,7 @@ INSERT INTO Person VALUES(7, "Vince Gilligan", DATE("1967-02-10"));
 INSERT INTO Person VALUES(8, "Random Guy", DATE("1950-09-16"));
 INSERT INTO Person VALUES(9, "John Williams", DATE("1932-02-08"));
 INSERT INTO Person VALUES(10, "Howard Shore", DATE("1946-10-18"));
+INSERT INTO Person VALUES(11, "Jessica Mireie", DATE("2000-04-09"));
 INSERT INTO Celebrity VALUES(2, 1.73, "I like sql");
 INSERT INTO Celebrity VALUES(3, 1.83, "Todd Phillips was born on December 20, 1970 in Brooklyn, New York City, New York, USA as Todd Bunzl. He is a producer and director, known for Joker (2019), A Tempo e Horas (2010) and Dias de Loucura (2003).");
 INSERT INTO Celebrity VALUES(4, NULL, "Bronson Webb was born on February 20, 1983 in London, England as Bronson John Webb. He is an actor, known for RocknRolla: A Quadrilha (2008), Reino dos Céus (2005) and Expiação (2007).");
@@ -44,6 +50,7 @@ INSERT INTO Celebrity VALUES(10, 1.79, "Howard Shore is a Canadian composer, bor
 INSERT INTO User VALUES(1, "up201806680@fe.up.pt", "http://link.to.photo", "verysecret");
 INSERT INTO User VALUES(5, "up201806134@fe.up.pt", "http://link.to.photo", "password");
 INSERT INTO User VALUES(8, "random.email@gmail.com", NULL, "donttellanyone");
+INSERT INTO User VALUES(11,"up201806723@fe.up.pt",NULL,"danger1234");
 
 -- ROLE
 
@@ -63,22 +70,28 @@ INSERT INTO Review VALUES(1, 1, NULL, NULL, 9);
 INSERT INTO Review VALUES(2, 5, "A very thrilling episode!", NULL, 10);
 INSERT INTO Review VALUES(3, 8, "Very nice!", "I really enjoyed it.", 8);
 INSERT INTO Review VALUES(4, 1, NULL, NULL, 10);
+INSERT INTO Review VALUES(5, 11, "German masterpiece!", NULL, 10);
+INSERT INTO Review VALUES(6, 11, NULL, NULL, 8);
 INSERT INTO ReviewProduction VALUES(1, 3);
 INSERT INTO ReviewProduction VALUES(3, 6);
 INSERT INTO ReviewProduction VALUES(4, 5);
+INSERT INTO ReviewProduction VALUES(5, 7);
 INSERT INTO ReviewEpisode VALUES(2, 1);
+INSERT INTO ReviewEpisode VALUES(6, 4);
 
 -- LIST
 
 INSERT INTO List VALUES(1, "My favorite Movies and Series", 1, 1);
 INSERT INTO List VALUES(2, "Best movies to watch in quarantine", 8, 0);
+INSERT INTO List VALUES(3, "My to watch list", 11, 1);
 INSERT INTO BelongsToList VALUES(1, 1);
 INSERT INTO BelongsToList VALUES(1, 2);
 INSERT INTO BelongsToList VALUES(2, 1);
 INSERT INTO BelongsToList VALUES(2, 4);
 INSERT INTO BelongsToList VALUES(2, 6);
 INSERT INTO BelongsToList VALUES(2, 3);
-
+INSERT INTO BelongsToList VALUES(3, 7);
+INSERT INTO BelongsToList VALUES(3, 6);
 -- GENRE
 
 INSERT INTO Genre VALUES(1, "Fantasy");
@@ -89,6 +102,7 @@ INSERT INTO Genre VALUES(5, "Drama");
 INSERT INTO Genre VALUES(6, "Thriller");
 INSERT INTO Genre VALUES(7, "Crime");
 INSERT INTO Genre VALUES(8, "Medieval");
+INSERT INTO Genre VALUES(9, "Mystery");
 
 INSERT INTO BelongsToGenre VALUES(1, 1);
 INSERT INTO BelongsToGenre VALUES(1, 2);
@@ -100,20 +114,22 @@ INSERT INTO BelongsToGenre VALUES(3, 5);
 INSERT INTO BelongsToGenre VALUES(3, 7);
 INSERT INTO BelongsToGenre VALUES(4, 2);
 INSERT INTO BelongsToGenre VALUES(4, 7);
+INSERT INTO BelongsToGenre VALUES(7, 7);
+INSERT INTO BelongsToGenre VALUES(7, 5);
+INSERT INTO BelongsToGenre VALUES(7, 9);
+
 
 -- AWARD
 
 INSERT INTO AwardType VALUES(1, "Oscar");
 INSERT INTO AwardType VALUES(2, "Grammy");
 INSERT INTO AwardType VALUES(3, "Golden Globe");
-
 INSERT INTO AwardCategory VALUES(1, "Best Performance by an Actor in a Leading Role");
 INSERT INTO AwardCategory VALUES(3, "Best Performance by an Actor in a Supporting Role");
 INSERT INTO AwardCategory VALUES(2, "Best Performance by an Actor in a Motion Picture");
 INSERT INTO AwardCategory VALUES(4, "Best Motion Picture of the Year");
 INSERT INTO AwardCategory VALUES(5, "Best Cinematography");
 INSERT INTO AwardCategory VALUES(6, "Best Music, Original Score");
-
 INSERT INTO Award VALUES(1, 2020, 3, 1, 1, 2);
 INSERT INTO NomineeProduction VALUES(1, 3);
 INSERT INTO NomineeCelebrity VALUES(1,2);
@@ -125,34 +141,6 @@ INSERT INTO NomineeProduction VALUES(3, 1);
 INSERT INTO Award VALUES(4, 2002, 1, 1, 6, 10);
 INSERT INTO NomineeCelebrity VALUES(4, 9);
 INSERT INTO NomineeProduction VALUES(4, 6);
-
-
-
-/*select * from Person;
-select * from Celebrity;
-select * from User;
-
-select * from Production;
-select * from Movie;
-select * from Series;
-select * from Season;
-select * from Episode;
-
-select * from Actor;
-select * from Role;
-select * from SingleEpisodeActor;
-
-select * from Review;
-select * from ReviewProduction;
-
-select * from BelongsToList;
-select * from BelongsToGenre;
-
-select * from CelebrityAward;
-select * from ProductionAward;*/
-
-
-
 
 
 
