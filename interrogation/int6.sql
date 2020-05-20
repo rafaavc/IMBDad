3443989141ae9c14f58027dcaa33c46ee204703d
rafaavc <rafavc2508@gmail.com>
2020-05-20 22:34:15 +0100

    wasn't able to make int6, started int7 but didnt have time to finish

--- interrogation/int6.sql
+++ interrogation/int6.sql
@@ -2,3 +2,13 @@
 .headers	on
 .nullvalue	NULL
 
+-- Todos os utilizadores que deram a mesma pontuação à mesma produção / episódio
+
+
+
+
+select productionId, userId, rating 
+from ReviewProduction join Review on ReviewProduction.reviewId = Review.id
+
+
+
--- interrogation/int7.sql
+++ interrogation/int7.sql
@@ -2,3 +2,18 @@
 .headers	on
 .nullvalue	NULL
 
+/*
+Obter a pontuação de uma determinada produção, tendo em conta todas as reviews que foram feitas sobre a mesma. 
+(Para séries: (nEpisodeReview*avgEpisodeReview + nProductionReview*avgProductionReview)/(nEpisodeReview + nProductionReview), 
+para filmes: avgProductionReview)
+*/
+
+-- NOT FINISHED YET
+
+/*select (nEpisodeReview * avgEpisodeReview + nProductionReview*avgProductionReview)/(nEpisodeReview + nProductionReview) as result
+from (*/
+    select rating, title, id from
+        (((select seriesId, Episode.id as episodeId from Episode inner join Season on Episode.seasonId = Season.id where seriesId = 14) as s
+            inner join ReviewEpisode on s.episodeId = ReviewEpisode.episodeId) 
+            as re inner join Review on re.reviewId = Review.id)
+--)
