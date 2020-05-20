4d4fe2ca1cab12ffe8f7576c78340dce1c0a1c61
rafaavc <rafavc2508@gmail.com>
2020-05-20 23:11:43 +0100

    update

--- interrogation/int7.sql
+++ interrogation/int7.sql
@@ -3,19 +3,17 @@
 .nullvalue	NULL
 
 /*
-Obter a pontuação de uma determinada produção, tendo em conta todas as reviews que foram feitas sobre a mesma. 
-(Para séries: (nEpisodeReview*avgEpisodeReview + nProductionReview*avgProductionReview)/(nEpisodeReview + nProductionReview), 
-para filmes: avgProductionReview)
+Obter a pontuação de uma determinada produção, tendo em conta todas as reviews que foram feitas sobre a mesma.
+No caso das séries, ter também em conta as reviews aos episódios
 */
 
 
 select cast(sum(rating) as float)/count(rating) as result from
-    (select rating, seriesId as pId/*, title, id as seasonId, seriesId, epId as episodeId*/ from
+    (select rating, seriesId as pId from
         (((select seriesId, Episode.id as epId from Episode inner join Season on Episode.seasonId = Season.id where seriesId = 39) as s
             inner join ReviewEpisode on s.epId = ReviewEpisode.episodeId) 
             as re inner join Review on re.reviewId = Review.id)
     union all
     select rating, Production.id as pId from
         (ReviewProduction inner join Production on Production.id = ReviewProduction.productionId) as rp
-            inner join Review on Review.id = rp.reviewId where Production.id = 39)
---)
+            inner join Review on Review.id = rp.reviewId where Production.id = 39);
