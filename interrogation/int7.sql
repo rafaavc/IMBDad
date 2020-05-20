44581c25a4a227f17ad7a866b830535a77db3a7a
rafaavc <rafavc2508@gmail.com>
2020-05-20 23:09:01 +0100

    int7 done i think

--- interrogation/int7.sql
+++ interrogation/int7.sql
@@ -8,12 +8,14 @@ Obter a pontuação de uma determinada produção, tendo em conta todas as revie
 para filmes: avgProductionReview)
 */
 
--- NOT FINISHED YET
 
-/*select (nEpisodeReview * avgEpisodeReview + nProductionReview*avgProductionReview)/(nEpisodeReview + nProductionReview) as result
-from (*/
-    select rating, title, id from
-        (((select seriesId, Episode.id as episodeId from Episode inner join Season on Episode.seasonId = Season.id where seriesId = 14) as s
-            inner join ReviewEpisode on s.episodeId = ReviewEpisode.episodeId) 
+select cast(sum(rating) as float)/count(rating) as result from
+    (select rating, seriesId as pId/*, title, id as seasonId, seriesId, epId as episodeId*/ from
+        (((select seriesId, Episode.id as epId from Episode inner join Season on Episode.seasonId = Season.id where seriesId = 39) as s
+            inner join ReviewEpisode on s.epId = ReviewEpisode.episodeId) 
             as re inner join Review on re.reviewId = Review.id)
+    union all
+    select rating, Production.id as pId from
+        (ReviewProduction inner join Production on Production.id = ReviewProduction.productionId) as rp
+            inner join Review on Review.id = rp.reviewId where Production.id = 39)
 --)
