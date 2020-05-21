c5bcc2df7c69a0f8286e384830f7faf4b029c07c
Jessica <jessymireie@github.com>
2020-05-21 17:17:55 +0100

    int8.sql

--- interrogation/int8.sql
+++ interrogation/int8.sql
@@ -2,3 +2,34 @@
 .headers	on
 .nullvalue	NULL
 
+--Utilizadores que partilhem pelo menos 1 production nas suas listas.
+
+SELECT DISTINCT Person.name, BelongsToList.productionId 
+FROM Person, BelongsToList 
+WHERE Person.id IN (
+	SELECT DISTINCT userId 
+	FROM List, BelongsToList
+	WHERE  List.id IN (
+	SELECT DISTINCT BL1.listId 
+	FROM BelongsToList AS BL1, BelongsToList AS BL2
+	WHERE BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId 
+	ORDER BY BL1.productionId ASC)
+	AND BelongsToList.productionId in (
+	SELECT DISTINCT BL1.productionId
+	FROM BelongsToList AS BL1, BelongsToList AS BL2
+	WHERE BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
+	)
+AND BelongsToList.productionId IN (
+	SELECT DISTINCT BelongsToList.productionId 
+	FROM List, BelongsToList 
+	WHERE  List.id in (
+	SELECT DISTINCT BL1.listId 
+	FROM BelongsToList AS BL1, BelongsToList AS BL2
+	WHERE BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId 
+	ORDER BY BL1.productionId ASC)
+	AND BelongsToList.productionId IN (
+	SELECT DISTINCT BL1.productionId 
+	FROM BelongsToList AS BL1, BelongsToList AS BL2 
+	WHERE BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
+	)
+ORDER BY BelongsToList.productionId;
