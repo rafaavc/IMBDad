a082764e996ea1a5b585becdf0793cc2bb037abd
Xavier-Pisco <x.pisco@gmail.com>
2020-05-21 17:34:27 +0100

    Some refactoring for memory and time improvements on int8.sql

--- interrogation/int8.sql
+++ interrogation/int8.sql
@@ -8,28 +8,28 @@ SELECT DISTINCT Person.name, BelongsToList.productionId
 FROM Person, BelongsToList 
 WHERE Person.id IN (
 	SELECT DISTINCT userId 
-	FROM List, BelongsToList
+	FROM List, BelongsToList as BL1
 	WHERE  List.id IN (
-	SELECT DISTINCT BL1.listId 
-	FROM BelongsToList AS BL1, BelongsToList AS BL2
-	WHERE BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId 
-	ORDER BY BL1.productionId ASC)
-	AND BelongsToList.productionId in (
-	SELECT DISTINCT BL1.productionId
-	FROM BelongsToList AS BL1, BelongsToList AS BL2
-	WHERE BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
-	)
+        SELECT DISTINCT BL1.listId 
+        FROM BelongsToList AS BL2 
+        WHERE (BL1.listId != BL2.listId AND BL1.productionId==BL2.productionId)
+    ) AND BL1.productionId in (
+        SELECT DISTINCT BL1.productionId
+        FROM BelongsToList AS BL2 
+        WHERE (BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
+    )
+)
 AND BelongsToList.productionId IN (
-	SELECT DISTINCT BelongsToList.productionId 
-	FROM List, BelongsToList 
-	WHERE  List.id in (
-	SELECT DISTINCT BL1.listId 
-	FROM BelongsToList AS BL1, BelongsToList AS BL2
-	WHERE BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId 
-	ORDER BY BL1.productionId ASC)
-	AND BelongsToList.productionId IN (
 	SELECT DISTINCT BL1.productionId 
-	FROM BelongsToList AS BL1, BelongsToList AS BL2 
-	WHERE BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
+	FROM List, BelongsToList as BL1
+	WHERE  List.id in (
+        SELECT DISTINCT BL1.listId 
+        FROM BelongsToList AS BL2 
+        WHERE (BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
+    ) AND BL1.productionId IN (
+        SELECT DISTINCT BL1.productionId 
+        FROM BelongsToList AS BL2 
+        WHERE (BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
+        )
 	)
 ORDER BY BelongsToList.productionId;
