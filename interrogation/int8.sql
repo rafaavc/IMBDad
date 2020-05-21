a1208c0224b86c6a87fad1f947838e1d91eade4e
Xavier-Pisco <x.pisco@gmail.com>
2020-05-21 23:52:25 +0100

    Created a view to read int8.sql better

--- interrogation/int8.sql
+++ interrogation/int8.sql
@@ -4,32 +4,27 @@
 
 --Utilizadores que partilhem pelo menos 1 production nas suas listas.
 
+Create View DiferentListsWithSameProduction as 
+SELECT DISTINCT userId, productionId
+FROM List, BelongsToList as BL1
+WHERE  List.id IN (
+    SELECT DISTINCT BL1.listId 
+    FROM BelongsToList AS BL2 
+    WHERE (BL1.listId != BL2.listId AND BL1.productionId==BL2.productionId)
+) AND BL1.productionId in (
+    SELECT DISTINCT BL1.productionId
+    FROM BelongsToList AS BL2 
+    WHERE (BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
+);
+
 SELECT DISTINCT Person.name, BelongsToList.productionId 
 FROM Person, BelongsToList 
 WHERE Person.id IN (
-	SELECT DISTINCT userId 
-	FROM List, BelongsToList as BL1
-	WHERE  List.id IN (
-        SELECT DISTINCT BL1.listId 
-        FROM BelongsToList AS BL2 
-        WHERE (BL1.listId != BL2.listId AND BL1.productionId==BL2.productionId)
-    ) AND BL1.productionId in (
-        SELECT DISTINCT BL1.productionId
-        FROM BelongsToList AS BL2 
-        WHERE (BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
-    )
+	SELECT userId
+    FROM DiferentListsWithSameProduction
 )
 AND BelongsToList.productionId IN (
-	SELECT DISTINCT BL1.productionId 
-	FROM List, BelongsToList as BL1
-	WHERE  List.id in (
-        SELECT DISTINCT BL1.listId 
-        FROM BelongsToList AS BL2 
-        WHERE (BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
-    ) AND BL1.productionId IN (
-        SELECT DISTINCT BL1.productionId 
-        FROM BelongsToList AS BL2 
-        WHERE (BL1.listId!=BL2.listId AND BL1.productionId==BL2.productionId)
-        )
-	)
+    SELECT productionId
+    FROM DiferentListsWithSameProduction
+)
 ORDER BY BelongsToList.productionId;
