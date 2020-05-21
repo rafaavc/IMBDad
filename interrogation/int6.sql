d8d2f91c61b957751d1081d186d0f8c77c411364
Xavier-Pisco <x.pisco@gmail.com>
2020-05-21 16:50:02 +0100

    Fixed int6.sql

--- interrogation/int6.sql
+++ interrogation/int6.sql
@@ -5,10 +5,18 @@
 -- Todos os utilizadores que deram a mesma pontuação à mesma produção / episódio
 
 
-
-
-select productionId, userId, rating 
-from ReviewProduction join Review on ReviewProduction.reviewId = Review.id
-
-
-
+Select Person.name, Review.rating 
+from Person, Review 
+where Person.id=Review.userId AND Review.id in (
+    Select  R1.id
+    FROM Review as R1, Review as R2 on (R1.rating = R2.rating and R1.userId != R2.userId)
+    WHERE R1.id in (
+        Select ReviewProduction.reviewId
+        FROM ReviewProduction
+        WHERE ReviewProduction.productionId = 39
+    ) and R2.id in (
+        Select ReviewProduction.reviewId
+        FROM ReviewProduction
+        WHERE ReviewProduction.productionId = 39
+    )
+);
