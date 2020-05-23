d24dc3ae4d678d441d5fe846b3d2ea4ac54a2810
rafaavc <rafavc2508@gmail.com>
2020-05-23 23:45:37 +0100

    Improved gatilho2_verifica.sql

--- triggers/gatilho2_verifica.sql
+++ triggers/gatilho2_verifica.sql
@@ -5,7 +5,7 @@
 INSERT INTO Award VALUES(8, 2015, NULL, 4, 7, NULL);
 INSERT INTO Award VALUES(9, 2016, NULL, 1, 1, NULL);
 
-
+.print 'Before:'
 Select * FROM Award;
 
 
@@ -16,8 +16,11 @@ INSERT INTO NomineeProduction VALUES(9, 9);
 INSERT INTO NomineeCelebrity VALUES(9, 12);
 INSERT INTO NomineeCelebrity VALUES(9, 13);
 
-UPDATE Award SET productionId = 2 WHERE id = 8;
-UPDATE Award SET productionId = 8, celebId = 12 WHERE id = 9;
+UPDATE Award SET productionId = 3 WHERE id = 8; -- Nao deve alterar
+UPDATE Award SET productionId = 2 WHERE id = 8; -- Deve alterar
+UPDATE Award SET productionId = 8, celebId = 14 WHERE id = 9; -- Não deve alterar
+UPDATE Award SET productionId = 8, celebId = 12 WHERE id = 9;  -- Deve alterar
 
-
+.print ''
+.print 'After:'
 Select * FROM Award;
