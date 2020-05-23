ccd4556120e1cbc76794d7e72df1ef492f7f90ec
Xavier-Pisco <x.pisco@gmail.com>
2020-05-24 00:21:24 +0100

    Changed gatilho2_verifica.sql according to project specification

--- triggers/gatilho2_verifica.sql
+++ triggers/gatilho2_verifica.sql
@@ -2,22 +2,17 @@
 .headers	on
 .nullvalue	NULL
 
-INSERT INTO Award VALUES(8, 2015, NULL, 4, 7, NULL);
-INSERT INTO Award VALUES(9, 2016, NULL, 1, 1, NULL);
 
-
+.print 'Before:'
 Select * FROM Award;
 
-
+-- Cria uma nomeação a uma award existente e adiciona os vencedores dessa award
 INSERT INTO NomineeProduction VALUES(8, 2);
-INSERT INTO NomineeProduction VALUES(9, 8);
-INSERT INTO NomineeProduction VALUES(9, 9);
-
-INSERT INTO NomineeCelebrity VALUES(9, 12);
-INSERT INTO NomineeCelebrity VALUES(9, 13);
-
 UPDATE Award SET productionId = 2 WHERE id = 8;
-UPDATE Award SET productionId = 8, celebId = 12 WHERE id = 9;
 
+-- Dá erro ao criar uma Awards com vencedor ser este estar nomeado
+INSERT INTO Award VALUES(16, 2000, 31, 1, 1, 23);
 
+.print ''
+.print 'After:'
 Select * FROM Award;
