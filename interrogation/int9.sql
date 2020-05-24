fd3856d821e4e4d195539860235a75f401998c12
Xavier-Pisco <x.pisco@gmail.com>
2020-05-24 12:20:56 +0100

    Did int9.sql

--- interrogation/int9.sql
+++ interrogation/int9.sql
@@ -2,3 +2,14 @@
 .headers	on
 .nullvalue	NULL
 
+Select Person.name as CelebrityName, Production.name as ProdcutionName
+FROM Person, Production
+WHERE (Person.id ,Production.id) in (
+    SELECT celebId, movieId as productionId
+    FROM MovieRole
+    ) or (Person.id, Production.id) in (
+    Select celebId, seriesId as productionId
+    FROM (Episode, Season on seasonId = Season.Id), EpisodeRole on episodeId = Episode.id
+)
+Order by (Person.name);
+
